71c9f4bcae80175f4be5b709a93adc4e35ae1350
DeanSergeyevich <109577115+DeanSergeyevich@users.noreply.github.com>
2024-02-25 20:04:48 +0300

    Add files via upload

---
+++ 25.02.2024/OOP homework1.2/OOP homework1.2/OOP homework1.2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class String
+{
+private:
+    static int count; 
+    char* buffer; 
+    size_t length; 
+
+public:
+   
+    String() : String(80) {}
+
+    
+    explicit String(size_t size) {
+        buffer = new char[size + 1]; // +1 для символа завершения строки
+        length = size;
+        ++count; // Увеличиваем счетчик созданных объектов
+    }
+
+   
+    String(const char* str) {
+        length = strlen(str);
+        buffer = new char[length + 1];
+        strcpy_s(buffer, length + 1, str);
+        ++count; // Увеличиваем счетчик созданных объектов
+    }
+    
+    ~String() {
+        delete[] buffer;
+        --count; // Уменьшаем счетчик созданных объектов при удалении объекта
+    }
+
+   
+    void input() {
+        std::cout << "Введите строку: ";
+        std::cin.getline(buffer, length + 1);
+    }
+
+    
+    void display() const {
+        std::cout << "Строка: " << buffer << std::endl;
+    }
+
+   
+    static int getCount() {
+        return count;
+    }
+};
+
+
+int String::count = 0;
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    String str1; // Строка длиной 80 символов
+    String str2(100); // Строка длиной 100 символов
+    String str3("Hello, World!"); // Строка, инициализированная строкой от пользователя
+
+    // Выводим созданные строки
+    str1.display();
+    str2.display();
+    str3.display();
+
+    // Вводим строку для третьего объекта
+    str3.input();
+    str3.display();
+
+    // Выводим количество созданных объектов
+    std::cout << "Количество созданных объектов: " << String::getCount() << std::endl;
+}
+
+
---
+++ 25.02.2024/OOP homework1/OOP homework1/OOP homework1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Fraction
+{
+private:
+    int num;
+    int denum;
+public:
+    Fraction(int num, int denum)
+    {
+        this->num = num;
+        this->denum = denum;
+    }
+
+    void show()
+    {
+            cout  << num << "/" << denum << endl;
+    }
+
+    Fraction add(const Fraction& other)
+    {
+        int new_num = num * other.denum + other.num * denum;
+        int new_denum = denum * other.denum;
+        return Fraction(new_num, new_denum);
+    }
+
+    Fraction subtract(const Fraction& other)
+    {
+        int new_num = num * other.denum - other.num * denum;
+        int new_denum = denum * other.denum;
+        return Fraction(new_num, new_denum);
+    }
+
+    Fraction multiply(const Fraction& other)
+    {
+        int new_num = num * other.num;
+        int new_denum = denum * other.denum;
+        return Fraction(new_num, new_denum);
+    }
+
+    Fraction divide(const Fraction& other)
+    {
+        int new_num = num * other.denum;
+        int new_denum = denum * other.num;
+        return Fraction(new_num, new_denum);
+    }
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    
+    Fraction num1(3, 6);
+    Fraction num2(23, 10);
+    num1.show();
+    num2.show();
+
+    Fraction sum = num1.add(num2);
+    Fraction difference = num1.subtract(num2);
+    Fraction product = num1.multiply(num2);
+    Fraction quotient = num1.divide(num2);
+
+    cout << "Сумма: ";
+    sum.show();
+    cout << "Разность: ";
+    difference.show();
+    cout << "Произведение: ";
+    product.show();
+    cout << "Частное: ";
+    quotient.show();
+
+    return 0;
+}
+
+
